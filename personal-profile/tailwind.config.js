bd4bdd13e6299a00bd73315215329ce80ec74b33
Muhammad Hanif <muhammadhanifkanan@gmail.com>
2025-10-29 22:52:33 +0700

    Menambahkan konfigurasi warna dan jenis font pada tailwind, serta menambahkan dependensi lucide react

--- personal-profile/tailwind.config.js
+++ personal-profile/tailwind.config.js
@@ -1,10 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // Tambahkan Font Poppins
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      // Tambahkan Warna Utama dari Desain Figma
+      colors: {
+        "dark-blue": "#232946",
+        "grayish-blue": "#5A5F73",
+        "primary-purple": "#4300FF",
+        "primary-pink": "#00FFDE",
+        "border-gray": "#E5E7EB",
+      },
+      //Background/Button Gradient
+      backgroundImage: {
+        "hero-gradient": "linear-gradient(90deg, #E0EAFE 0%, #F9E5FF 100%)",
+        "btn-gradient": "linear-gradient(90deg, #4300FF 0%, #00FFDE 100%)",
+      },
+    },
   },
   plugins: [],
-}
+};
