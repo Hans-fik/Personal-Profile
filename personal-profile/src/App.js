26497de96f71ad97fb6e2130ef09bf7104b3c6f2
Muhammad Hanif <muhammadhanifkanan@gmail.com>
2025-10-29 18:49:53 +0700

    Mencoba mengimplementasikan contoh profile card (sukses)

--- personal-profile/src/App.js
+++ personal-profile/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import ProfileCard from './components/ProfileCard';
+import AboutSection from './components/AboutSection';
+import SkillsSection from './components/SkillsSection';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font-sans bg-gray-50 min-h-screen">
+      <main className="px-6 py-10">
+        <ProfileCard />
+      </main>
     </div>
   );
 }
